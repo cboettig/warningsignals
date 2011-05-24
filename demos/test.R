b5f73e2483e53edb2abf571f9d1d0819b4a6a414
Carl Boettiger <cboettig@gmail.com>
2011-05-24 11:47:34 -0700

    autocommit

--- demos/test.R
+++ demos/test.R
@@ -5,7 +5,7 @@ tags="stochpop, warningsignals"
 data(ibms)
 
 
-Rprof("boot.out")
+#Rprof("boot.out")
 deterior_m<-fit_models(ibm_critical, "LSN")
 mc1 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=20, times = 20)
 
