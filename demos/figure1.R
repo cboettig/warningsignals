d9511c7d95592eae010031ff40b089fc78a99ffd
Carl Boettiger <cboettig@gmail.com>
2011-05-24 08:45:27 -0700

    trying with simulated example

--- demos/figure1.R
+++ demos/figure1.R
@@ -1,24 +1,26 @@
 # figure1.R
 require(warningsignals)
+tags="stochpop, warningsignals"
+data(ibms)
+social_plot(all_indicators(list(deteriorating=ibm_critical, stable=ibm_stable)), tags=tags)
 
-#data(ibms)
-#all_indicators(list(deteriorating=ibm_critical, stable=ibm_stable))
-
-data(drake)
-H6 <- drake_deterior[["H6"]]
-all_indicators(list(Daphnia=H6), indicators=c("Var", "Autocor"))
-deterior_m<-fit_models(H6, "LSN")
+deterior_m<-fit_models(ibm_critical, "LSN")
 deterior_taus <- bootstrap_tau(deterior_m$X, deterior_m$const, 
                                deterior_m$timedep, cpu=2, nboot=2)
 
-mc1 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200)
-mc2 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times=50)
-mc3 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times=100)
-mc4 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times=200)
 
+mc1 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times = 20)
 require(socialR)
-social_plot(plot(mc1))
-social_plot(plot(mc2))
-social_plot(plot(mc3))
-social_plot(plot(mc4))
+social_plot(plot(mc1), tag=tags, comment="20 sample pts")
+mc2 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times=50)
+social_plot(plot(mc2), tag=tags, comment="50 sample pts")
+mc3 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times=100)
+social_plot(plot(mc3), tag=tags, comment="100 sample pts")
+mc4 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times=200)
+social_plot(plot(mc4), tag=tags, comment="200 sample pts")
+mc5 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times=400)
+social_plot(plot(mc5), tag=tags, comment="400 sample pts")
+
+#data(drake)
+#H6 <- drake_deterior[["H6"]]
 
