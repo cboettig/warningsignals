49b686d06ecc7fd97e1c667bd1efe3073c3b5d10
Carl Boettiger <cboettig@gmail.com>
2011-05-23 21:48:10 -0700

    updated example for a run on zero

--- demos/figure1.R
+++ demos/figure1.R
@@ -1,31 +1,24 @@
 # figure1.R
 require(warningsignals)
 
-data(ibms)
-all_indicators(list(deteriorating=ibm_critical, stable=ibm_stable))
+#data(ibms)
+#all_indicators(list(deteriorating=ibm_critical, stable=ibm_stable))
 
-deterior_m<-fit_models(ibm_critical, "LSN")
-constant_m<-fit_models(ibm_stable, "LSN")
-
-deterior_taus <- bootstrap_tau(deterior_m$X, deterior_m$const, 
-                               deterior_m$timedep)
-constant_taus <- bootstrap_tau(constant_m$X, constant_m$const,
-                               constant_m$timedep)
-
-#deterior_mc <- montecarlotest(deterior_m$const, deterior_m$timedep, times=10)
-
-constant_mc <- montecarlotest(constant_m$const, constant_m$timedep, times=10)
-
-
-
-
-require(warningsignals)
 data(drake)
 H6 <- drake_deterior[["H6"]]
 all_indicators(list(Daphnia=H6), indicators=c("Var", "Autocor"))
 deterior_m<-fit_models(H6, "LSN")
 deterior_taus <- bootstrap_tau(deterior_m$X, deterior_m$const, 
                                deterior_m$timedep, cpu=2, nboot=2)
-deterior_mc <- montecarlotest(deterior_m$const, deterior_m$timedep, times=10, cpu=2, nboot=2)
 
+mc1 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200)
+mc2 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times=50)
+mc3 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times=100)
+mc4 <- montecarlotest(deterior_m$const, deterior_m$timedep, cpu=16, nboot=200, times=200)
+
+require(socialR)
+social_plot(plot(mc1))
+social_plot(plot(mc2))
+social_plot(plot(mc3))
+social_plot(plot(mc4))
 
