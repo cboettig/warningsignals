958f4dbee09261f56bfc18fab10b3ca6e0a71a31
Carl Boettiger <cboettig@gmail.com>
2011-05-26 06:36:48 -0700

    autocommit

--- demos/mcmc.R
+++ demos/mcmc.R
@@ -27,7 +27,7 @@ sfLibrary(warningsignals)
 sfExportAll()
 
 chains <- mcmcmc_fn(pars, f, prior, MaxTime=1e6, indep=1e4, 
-                    stepsizes=c(0.1, 0.00005, 10, 0.1), Delta_T=2)
+                    stepsizes=c(0.1, 0.00001, 10, 0.1), Delta_T=2)
 burnin=1e3
 
 
