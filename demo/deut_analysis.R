9395bb4ac6b964088cb67d2147a2450390a8972a
Carl Boettiger <cboettig@gmail.com>
2011-06-16 14:59:30 -0700

    deut_analysis.R  autocommit

--- demo/deut_analysis.R
+++ demo/deut_analysis.R
@@ -1,4 +1,5 @@
 # analysis.R
+rm(list=ls())
 require(warningsignals)
 require(socialR)
 script <- "deut_analysis.R"
@@ -12,18 +13,15 @@ nboot <- 160
 freq <- c(.1, .5, 1.5, 2, 5)
 
 source("analysis.R")
-
-
 data(deuterium)
-deut <- lapply(deuterium, 
-               function(x) analysis(x, cpu=cpu, nboot=nboot, freq=freq))
-save(list="deut", file="deut_analysis.rda")
-
+deut <- vector("list", length=3)
 for(i in 1:length(deut)){
-png("deut[[i]]_roc.png"); compare_roc_curves(deut[[i]]$taus, deut[[i]]$mc); dev.off()
-upload("deut[[i]]_roc.png", script=script, tags=tags)
-png("deut[[i]]_sampling.png"); plot_sampling_freq(deut[[i]]$sampling, deut[[i]]$freq); dev.off()
-upload("deut[[i]]_sampling.png", script=script, tags=tags)
+  deut[[i]] <- analysis(deuterium[[i]], cpu=cpu, nboot=nboot, freq=freq)
+  png("deut[[i]]_roc.png"); compare_roc_curves(deut[[i]]$taus, deut[[i]]$mc); dev.off()
+  upload("deut[[i]]_roc.png", script=script, tags=tags)
+  png("deut[[i]]_sampling.png"); plot_sampling_freq(deut[[i]]$sampling, deut[[i]]$freq); dev.off()
+  upload("deut[[i]]_sampling.png", script=script, tags=tags)
 }
 
+save(list="deut", file="deut_analysis.rda")
 
