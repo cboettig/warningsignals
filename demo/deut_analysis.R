8f42ba7c691d20268c9c687cf7ccc3ac3907e066
Carl Boettiger <cboettig@gmail.com>
2011-06-23 15:51:33 -0700

    deut_analysis.R  autocommit

--- demo/deut_analysis.R
+++ demo/deut_analysis.R
@@ -1,12 +1,15 @@
-# analysis.R
+# deut_analysis.R
 rm(list=ls())
 require(warningsignals)
+
+###############
 require(socialR)
 script <- "deut_analysis.R"
-gitcommit(script)
+gitaddr <- gitcommit(script)
 tags="warningsignals, stochpop"
 tweet_errors(script, tags=tags)
 on.exit(system("git push")) 
+###############
 
 cpu <- 16
 nboot <- 160
@@ -17,10 +20,11 @@ data(deuterium)
 deut <- vector("list", length=3)
 for(i in 1:length(deut)){
   deut[[i]] <- analysis(deuterium[[i]], cpu=cpu, nboot=nboot, freq=freq)
-  png("deut[[i]]_roc.png"); compare_roc_curves(deut[[i]]$taus, deut[[i]]$mc); dev.off()
-  upload("deut[[i]]_roc.png", script=script, tags=tags)
-  png("deut[[i]]_sampling.png"); plot_sampling_freq(deut[[i]]$sampling, deut[[i]]$freq); dev.off()
-  upload("deut[[i]]_sampling.png", script=script, tags=tags)
+  png(paste("deut_", i, "_roc.png", sep="")); 
+  compare_roc_curves(deut[[i]]$taus, deut[[i]]$mc); dev.off()
+  upload(paste("deut_", i, "_roc.png", sep=""), script=script, gitaddr=gitaddr, tags=tags)
+  png(paste("deut_", i, "_sampling.png", sep="")); plot_sampling_freq(deut[[i]]$sampling, deut[[i]]$freq); dev.off()
+  upload(paste("deut_", i, "_sampling.png", sep=""), script=script, gitaddr=gitaddr, tags=tags)
 }
 
 save(list="deut", file="deut_analysis.rda")
