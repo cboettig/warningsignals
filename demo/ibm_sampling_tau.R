6982539b55d0b1bad90f0eb3504b295acc7b71d8
Carl Boettiger <cboettig@gmail.com>
2011-06-29 15:58:01 -0700

    ibm_sampling_tau.R  autocommit

--- demo/ibm_sampling_tau.R
+++ demo/ibm_sampling_tau.R
@@ -22,11 +22,11 @@ plot_tau_sampling_freq <- function(sampling_all, freq){
   sampling <- reformat_tau_dists(sampling_vars)
 
   auc <- numeric(length(sampling))
-  auc[1] <- roc_curve(sampling[[1]], lwd=2, col=1)
+  auc[1] <- roc_curve(sampling[[1]], lwd=2, col=1, pts=10)
   legend_txt <- character(length(sampling))
   legend_txt[1] <- paste("Effort", freq[1], "AUC =",round(auc[1],3))
   for(i in 2:length(sampling)){
-    auc[i] <- roc_curve(sampling[[i]], lwd=2, col=i, add=TRUE)
+    auc[i] <- roc_curve(sampling[[i]], lwd=2, col=i, add=TRUE, pts=10)
     legend_txt[i] <- paste("Effort", freq[i], "AUC =",round(auc[i],3))
   }
   legend("bottomright",legend_txt, col=c(1:length(sampling)), lty=1, lwd=3) 
@@ -39,9 +39,11 @@ m <- fit_models(ibm_critical, "LSN")
 if(m$timedep$pars[['m']] > 0)
   stop("m > 0")
 
-sampling <- indicator_sampling_freq(m, cpu, nboot, sample_effort=freq,
-                               length.original=length(m$X)) 
-save(list=ls(), file="ibm_sampling_tau.Rdat")
+#sampling <- indicator_sampling_freq(m, cpu, nboot, sample_effort=freq,
+#                               length.original=length(m$X)) 
+#save(list=ls(), file="ibm_sampling_tau.Rdat")
+load("ibm_sampling_tau.Rdat")
+
 png("tau_sampling.png")
 plot_tau_sampling_freq(sampling, freq)
 dev.off()
