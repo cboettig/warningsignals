df1d2b12c065a6ec8f98255ed221c7bc43d0c0e7
Carl Boettiger <cboettig@gmail.com>
2011-06-29 14:21:21 -0700

    ibm_sampling_tau.R  autocommit

--- demo/ibm_sampling_tau.R
+++ demo/ibm_sampling_tau.R
@@ -21,12 +21,27 @@ freq <- c(1, 2, 10) #c(.1, .5, 1.5, 2, 5)
 source("analysis.R")
 
 
+plot_sampling_freq <- function(sampling, freq){
+  auc <- numeric(length(sampling))
+  auc[1] <- roc_curve(sampling[[1]], lwd=2, col=1)
+  legend_txt <- character(length(sampling))
+  legend_txt[1] <- paste("Effort", freq[1], "AUC =",round(auc[1],3))
+  for(i in 2:length(sampling)){
+    sampling[[i]] <- remove_unconverged(sampling[[i]])
+    auc[i] <- roc_curve(sampling[[i]], lwd=2, col=i, add=TRUE)
+    legend_txt[i] <- paste("Effort", freq[i], "AUC =",round(auc[i],3))
+  }
+  legend("bottomright",legend_txt, col=c(1:length(sampling)), lty=1, lwd=3) 
+}
+
+
+
 data(ibms)
 m <- fit_models(ibm_critical, "LSN")
 sampling <- indicator_sampling_freq(m, cpu, nboot, sample_effort=freq,
                                length.original=length(m$X)) 
 
-save(list=ls(), file="ibm_sampling_tau.png")
+save(list=ls(), file="ibm_sampling_tau.Rout")
 
 
 png("tau_sampling.png")
