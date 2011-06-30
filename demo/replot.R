011bbaf0d397ebd0dc80c423587955832e579420
Carl Boettiger <cboettig@gmail.com>
2011-06-30 16:13:00 -0700

    replot.R  autocommit

--- demo/replot.R
+++ demo/replot.R
@@ -2,8 +2,8 @@
 rm(list=ls())
 require(warningsignals)
 
-load("~/flickr/5888212714.Rdat")
-
+#load("~/flickr/5888212714.Rdat")
+load("ibm_analysis.Rdat")
 ###############
 require(socialR)
 script <- "replot.R"
@@ -11,13 +11,20 @@ gitaddr <- gitcommit(script)
 tags="warningsignals, stochpop"
 tweet_errors(script, tags=tags)
 ###############
-source("analysis.R")
 
 
 
-mc <- remove_unconverged(deut[[1]]$mc)
-taus <- reformat_tau_dists(deut[[1]]$taus) 
+## Original plot
+png("ibm_crit_roc.png"); plot_roc_curves(c(list(mc), taus)); dev.off()
+upload("ibm_crit_roc.png", script=script, gitaddr=gitaddr, tags=tags)
 
-png("deut1_roc.png"); plot_roc_curves(c(list(mc), taus)); dev.off()
-upload("deut1_roc.png", script=script, gitaddr=gitaddr, tags=tags)
+## plots at increasing effort
+for(i in 1:length(freq)){
+  file <- paste("ibm_crit_", freq[i], ".png")
+  png(file); 
+  plot_roc_curves(c(sampling[[i]], indicator_sampling[[i]])); 
+  dev.off()
+  upload(file, script=script, gitaddr=gitaddr, tags=tags)
+}
+
 
