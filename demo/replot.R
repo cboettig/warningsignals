fced15d02fe2a48f09e2cb6b61585c4bf177316e
Carl Boettiger <cboettig@gmail.com>
2011-06-29 19:07:26 -0700

    replot.R  autocommit

---
+++ demo/replot.R
@@ -0,0 +1,20 @@
+# replot.R
+rm(list=ls())
+require(warningsignals)
+
+
+load("5882379329.Rdat")
+
+###############
+require(socialR)
+script <- "replot.R"
+gitaddr <- gitcommit(script)
+tags="warningsignals, stochpop"
+tweet_errors(script, tags=tags)
+###############
+
+
+png(paste("deut_", i, "_roc.png", sep="")); 
+compare_roc_curves(deut[[i]]$taus, deut[[i]]$mc); 
+dev.off()
+
