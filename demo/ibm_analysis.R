7b725dd3736f6ca98ecbd87c7b102ca19ee18c5b
Carl Boettiger <cboettig@gmail.com>
2011-06-30 14:26:38 -0700

    ibm_analysis.R  autocommit

--- demo/ibm_analysis.R
+++ demo/ibm_analysis.R
@@ -1,4 +1,5 @@
-# analysis.R
+# ibm_analysis.R
+rm(list=ls())
 require(warningsignals)
 require(socialR)
 script <- "ibm_analysis.R"
@@ -7,9 +8,10 @@ tags="warningsignals, stochpop"
 tweet_errors(script, tags=tags)
 on.exit(system("git push")) 
 
-cpu <- 4
-nboot <- 100
-freq <- c(.5, 1, 2, 5, 10)
+cpu <- 16
+nboot <- 16
+#freq <- c(25, 50, 100, 200, 500)
+freq <- c(25, 50)
 
 source("analysis.R")
 
@@ -17,26 +19,28 @@ source("analysis.R")
 data(ibms)
 m <- fit_models(ibm_critical, "LSN")
 sampling <- sampling_freq(m$const, m$timedep, cpu=cpu, nboot=nboot,
-                          sample_effort=freq, length.original=length(m$X))
-taus <- bootstrap_tau(m$X, m$const, m$timedep, cpu=cpu, nboot=nboot)
-mc <- montecarlotest(m$const, m$timedep, cpu=cpu, nboot=nboot)
+                          sample_effort=freq)
+taus <- reformat_tau_dists(bootstrap_tau(m$X, m$const, m$timedep, 
+                                         cpu=cpu, nboot=nboot))
+mc <- remove_unconverged(montecarlotest(m$const, m$timedep, 
+                                        cpu=cpu, nboot=nboot))
 indicator_sampling <- indicator_sampling_freq(m, cpu, nboot,
-                                              sample_effort=freq,
-                                              length.original=length(m$X)) 
+                                              sample_effort=freq) 
+
+
 
 ### Plot methods
 
-png("ibm_crit_roc.png"); compare_roc_curves(taus, mc); dev.off()
+## Original plot
+png("ibm_crit_roc.png"); plot_roc_curves(c(taus, mc)); dev.off()
 upload("ibm_crit_roc.png", script=script, gitaddr=gitaddr, tags=tags)
 
-png("ibm_crit_sampling.png"); plot_sampling_freq(sampling, freq); dev.off()
-upload("ibm_crit_sampling.png", script=script, gitaddr=gitaddr, tags=tags)
-
-
-png("tau_sampling.png"); # variance at different sampling efforts
-plot_tau_sampling_freq(sampling, freq, pts=100, stat=1); 
-dev.off()
-upload("tau_sampling.png", script=script, gitaddr=gitaddr, tags=tags)
+## plots at increasing effort
+for(i in 1:length(freq)){
+  file <- paste("ibm_crit_", freq[i], ".png")
+  png(file); plot_roc_curves(c(sampling[[i]], indicator_sampling[[i]])); dev.off()
+  upload(file, script=script, gitaddr=gitaddr, tags=tags)
+}
 
 
 
