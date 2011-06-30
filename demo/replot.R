0ce77be8f8406e3a102e80305ae2cadf8bd36c42
Carl Boettiger <cboettig@gmail.com>
2011-06-30 15:48:20 -0700

    replot.R  autocommit

--- demo/replot.R
+++ demo/replot.R
@@ -13,8 +13,8 @@ tweet_errors(script, tags=tags)
 ###############
 source("analysis.R")
 
-mc <- remove_unconverged(deut[[1]]$mc)
-taus <- reformat_tau_dists(deut[[1]]$taus) 
+mc <- deut[[1]]$mc
+taus <- deut[[1]]$taus
 
 png("deut1_roc.png"); plot_roc_curves(c(mc, taus)); dev.off()
 upload("deut1_roc.png", script=script, gitaddr=gitaddr, tags=tags)
