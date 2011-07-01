0ef43426fd68b861b2e391dd0a5e01f6ddad4f13
Carl Boettiger <cboettig@gmail.com>
2011-06-30 17:32:34 -0700

    replot.R  autocommit

--- demo/replot.R
+++ demo/replot.R
@@ -19,13 +19,13 @@ for(i in 1:length(freq)){
   input <- c(sampling[i], indicator_sampling[[i]])
   file <- paste("ibm_crit_", freq[i], ".png", sep="")
   png(file); 
-#  plot_roc_curves(input); 
+  plot_roc_curves(input, cex.axis=3, cex.lab=3); 
   dev.off()
-#  upload(file, script=script, gitaddr=gitaddr, tags=tags)
+  upload(file, script=script, gitaddr=gitaddr, tags=tags)
 
   file <- paste("dist_ibm_crit_", freq[i], ".png", sep="")
   png(file, width=480*length(input))
-  plot_dists(input); 
+  plot_dists(input, cex.axis=3, cex.lab=3); 
   dev.off()
   upload(file, script=script, gitaddr=gitaddr, tags=tags)
 }
