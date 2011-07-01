40a8ba140eaca71b36d8adcf01ae83f8a81e9c3e
Carl Boettiger <cboettig@gmail.com>
2011-06-30 17:04:22 -0700

    replot.R  autocommit

--- demo/replot.R
+++ demo/replot.R
@@ -2,7 +2,7 @@
 rm(list=ls())
 require(warningsignals)
 
-load("~/flickr/5888212714.Rdat")
+load("~/flickr/5889474134.Rdat")
 ###############
 require(socialR)
 script <- "replot.R"
@@ -21,5 +21,21 @@ plot_dists <- function(objects, ...){
   }
 }
 
-plot_dists(c(sampling[i], indicator_sampling[[i]])); 
+
+## plots at increasing effort
+for(i in 1:length(freq)){
+  file <- paste("ibm_crit_", freq[i], ".png", sep="")
+  png(file); 
+  plot_roc_curves(c(sampling[i], indicator_sampling[[i]])); 
+  dev.off()
+  upload(file, script=script, gitaddr=gitaddr, tags=tags)
+
+  file <- paste("dist_ibm_crit_", freq[i], ".png", sep="")
+  png(file)
+  plot_dists(c(sampling[i], indicator_sampling[[i]])); 
+  dev.off()
+  upload(file, script=script, gitaddr=gitaddr, tags=tags)
+
+}
+
 
