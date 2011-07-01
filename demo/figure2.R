3a98a0c388ccbc7bbda6eaae643f800a97a72084
Carl Boettiger <cboettig@gmail.com>
2011-07-01 14:37:08 -0700

    figure2.R  autocommit

--- demo/figure2.R
+++ demo/figure2.R
@@ -13,7 +13,7 @@ data(drake)
 X <- list(Stable=ibm_stable, Deteriorating=ibm_critical, Glaciation=deuterium[[3]], Daphnia=drake_deterior$H6)
 
 png("indicators.png", width=4.6, units="in", height=2.3, res=1200)
-all_indicators(X)
+all_indicators(X, indicators="Autocor", method="kendall")
 dev.off()
 
 upload("indicators.png", script=script, gitaddr=gitaddr, tags=tags)
