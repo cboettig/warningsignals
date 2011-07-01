f890b2ffc1da2b437fef1ad60b87913ac7cd7162
Carl Boettiger <cboettig@gmail.com>
2011-06-30 17:48:11 -0700

    drake_analysis.R  autocommit

--- demo/drake_analysis.R
+++ demo/drake_analysis.R
@@ -1,22 +1,68 @@
 # analysis.R
 require(warningsignals)
+
+###############
 require(socialR)
 script <- "drake_analysis.R"
 gitaddr <- gitcommit(script)
 tags="warningsignals, stochpop"
 tweet_errors(script, tags=tags)
 on.exit(system("git push")) 
-
-freq <- c(2, 5, 10, 20, 40)
+##############
 source("analysis.R")
 
 data(drake)
-drake <- analysis(drake_deterior$H6, cpu=8, nboot=1000, freq=freq)
-save(list="drake", file="drake_analysis.rda")
 
-png("drake_roc.png"); compare_roc_curves(drake$taus, drake$mc); dev.off()
+m <- fit_models(drake_deterior, "LSN")
+
+
+cpu <- 16
+nboot <- 500
+freq <- c(25, 50, 100, 200, 500)
+
+
+## Run the Analyses
+sampling <- 
+sampling_freq(m$const, m$timedep, cpu=cpu,
+              nboot=nboot, sample_effort=freq)
+
+taus <- 
+reformat_tau_dists(
+  bootstrap_tau(m$X, m$const, m$timedep, 
+                cpu=cpu, nboot=nboot))
+
+mc <- 
+remove_unconverged(
+  montecarlotest(m$const, m$timedep, 
+                 cpu=cpu, nboot=nboot)) 
+
+indicator_sampling <- 
+indicator_sampling_freq(m, cpu, nboot,
+                        sample_effort=freq) 
+
+
+
+### Plot methods
+## Original plot
+png("drake_roc.png"); plot_roc_curves(c(list(mc), taus)); dev.off()
 upload("drake_roc.png", script=script, gitaddr=gitaddr, tags=tags)
-png("drake_sampling.png"); plot_sampling_freq(drake$sampling, drake$freq); dev.off()
-upload("drake_sampling.png", script=script, gitaddr=gitaddr, tags=tags)
+
+
+for(i in 1:length(freq)){
+  input <- c(sampling[i], indicator_sampling[[i]])
+  file <- paste("drake_", freq[i], ".png", sep="")
+  png(file); 
+  plot_roc_curves(input, cex.axis=2, cex.lab=2); 
+  dev.off()
+  upload(file, script=script, gitaddr=gitaddr, tags=tags)
+
+  file <- paste("dist_drake_", freq[i], ".png", sep="")
+  png(file, width=480*length(input))
+  plot_dists(input, cex.axis=3, cex.lab=3.5); 
+  dev.off()
+  upload(file, script=script, gitaddr=gitaddr, tags=tags)
+}
+
+
 
 
