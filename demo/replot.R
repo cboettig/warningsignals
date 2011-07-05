eb2b806a9e77359ecbc8df64b9668af4805e7612
Carl Boettiger <cboettig@gmail.com>
2011-07-05 16:30:32 -0700

    replot.R  autocommit

--- demo/replot.R
+++ demo/replot.R
@@ -10,28 +10,32 @@ tags="warningsignals, stochpop"
 tweet_errors(script, tags=tags)
 ###############
 
-load("~/flickr/5904550426.Rdat")
+load("~/flickr/5895991499.Rdat")
 
 cpu=16
+indicator_sampling <- indicator_sampling_freq(m, cpu, nboot,
+                                              sample_effort=freq) 
 
-indicator_sampling <- 
-indicator_sampling_freq(m, cpu, nboot,
-                        sample_effort=freq) 
+### Plot methods
+## Original plot
+png("ibm_crit_roc.png"); plot_roc_curves(c(list(mc), taus)); dev.off()
+upload("ibm_crit_roc.png", script=script, gitaddr=gitaddr, tags=tags)
+
+
 for(i in 1:length(freq)){
   input <- c(sampling[i], indicator_sampling[[i]])
-  file <- paste("deut_", freq[i], ".png", sep="")
+  file <- paste("ibm_crit_", freq[i], ".png", sep="")
   png(file); 
   plot_roc_curves(input, cex.axis=2, cex.lab=2); 
   dev.off()
   upload(file, script=script, gitaddr=gitaddr, tags=tags)
 
-  file <- paste("dist_deut_", freq[i], ".png", sep="")
+  file <- paste("dist_ibm_crit_", freq[i], ".png", sep="")
   png(file, width=480*length(input))
   plot_dists(input, cex.axis=3, cex.lab=3.5); 
   dev.off()
   upload(file, script=script, gitaddr=gitaddr, tags=tags)
 }
-
 
 
 
