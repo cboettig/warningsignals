2a4f0f85d2db0a2c41ac49548de53a045794ebe9
Carl Boettiger <cboettig@gmail.com>
2011-07-05 12:39:04 -0700

    replot.R  autocommit

--- demo/replot.R
+++ demo/replot.R
@@ -11,6 +11,11 @@ tweet_errors(script, tags=tags)
 ###############
 
 load("5905640579.Rdat")
+
+mt <- sapply(list(ibm_crit=ibm_crit, deut1=deut1, deut3=deut3, daphnia=daphnia, caco3=caco3, lin_deterior=lin_deterior, lin_const=lin_const), get.mt)
+
+
+
 png("all_fits.png", width=2*480)
 barplot(mt)
 dev.off()
