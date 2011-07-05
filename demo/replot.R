7ac138fcd4ddc3434f9128506a0849d1aaa24b69
Carl Boettiger <cboettig@gmail.com>
2011-07-05 15:53:16 -0700

    replot.R  autocommit

--- demo/replot.R
+++ demo/replot.R
@@ -12,8 +12,8 @@ tweet_errors(script, tags=tags)
 
 load("~/flickr/5904550426.Rdat")
 
-cpu=16
+cpu=1
 out_50 <-  bootstrap_tau(m$X, m$const, m$timedep, cpu=cpu, nboot=nboot, times=50)
 
 
-save("replot_50.Rdat")
+save(list=ls(), file="replot_50.Rdat")
