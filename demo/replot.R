b49efba5a3185f251f2b9bc8e83b2ae1c2483643
Carl Boettiger <cboettig@gmail.com>
2011-07-06 09:08:39 -0700

    replot.R  autocommit

--- demo/replot.R
+++ demo/replot.R
@@ -3,7 +3,7 @@ rm(list=ls())
 require(warningsignals)
 
 ## Load must come BEFORE socialR block ##
-load("~/flickr/5895991499.Rdat")
+load("~/flickr/5890668068.Rdat")
 
 
 
@@ -23,19 +23,19 @@ indicator_sampling <- indicator_sampling_freq(m, cpu, nboot,
 
 ### Plot methods
 ## Original plot
-png("ibm_crit_roc.png"); plot_roc_curves(c(list(mc), taus)); dev.off()
-upload("ibm_crit_roc.png", script=script, gitaddr=gitaddr, tags=tags)
+png("drake_roc.png"); plot_roc_curves(c(list(mc), taus)); dev.off()
+upload("drake_roc.png", script=script, gitaddr=gitaddr, tags=tags)
 
 
 for(i in 1:length(freq)){
   input <- c(sampling[i], indicator_sampling[[i]])
-  file <- paste("ibm_crit_", freq[i], ".png", sep="")
+  file <- paste("drake_", freq[i], ".png", sep="")
   png(file); 
   plot_roc_curves(input, cex.axis=2, cex.lab=2); 
   dev.off()
   upload(file, script=script, gitaddr=gitaddr, tags=tags)
 
-  file <- paste("dist_ibm_crit_", freq[i], ".png", sep="")
+  file <- paste("dist_drake_", freq[i], ".png", sep="")
   png(file, width=480*length(input))
   plot_dists(input, cex.axis=3, cex.lab=3.5); 
   dev.off()
