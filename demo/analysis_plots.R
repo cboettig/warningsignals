6df11ad29fb3e95f467337c4d00b00718c5a0596
Carl Boettiger <cboettig@gmail.com>
2011-07-06 13:55:41 -0700

    analysis_plots.R  autocommit

---
+++ demo/analysis_plots.R
@@ -0,0 +1,41 @@
+# analysis_plots.R
+rm(list=ls())
+
+load("caco3.Rdat")
+
+###########################
+require(socialR)
+script <- "analysis_plots.R"
+gitaddr <- gitcommit(script)
+tags="warningsignals, stochpop"
+tweet_errors(script, tags=tags)
+on.exit(system("git push")) 
+###########################
+
+require(warningsignals)
+
+indicator_sampling <- 
+indicator_sampling_freq(m, cpu, nboot,
+                        sample_effort=freq) 
+
+
+png("caco3_roc.png"); plot_roc_curves(c(list(mc), taus)); dev.off()
+upload("caco3_roc.png", script=script, gitaddr=gitaddr, tags=tags)
+
+for(i in 1:length(freq)){
+  input <- c(sampling[i], indicator_sampling[[i]])
+  file <- paste("caco3_", freq[i], ".png", sep="")
+  png(file); 
+  plot_roc_curves(input, cex.axis=2, cex.lab=2); 
+  dev.off()
+  upload(file, script=script, gitaddr=gitaddr, tags=tags)
+
+#  file <- paste("dist_caco3_", freq[i], ".png", sep="")
+#  png(file, width=480*length(input))
+#  plot_dists(input, cex.axis=3, cex.lab=3.5); 
+#  dev.off()
+#  upload(file, script=script, gitaddr=gitaddr, tags=tags)
+}
+
+
+
