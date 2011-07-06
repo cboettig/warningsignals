52bfb9fb295d5355573b12975b6a8b40feff3bfd
Carl Boettiger <cboettig@gmail.com>
2011-07-06 15:34:32 -0700

    analysis_plots.R  autocommit

--- demo/analysis_plots.R
+++ demo/analysis_plots.R
@@ -1,7 +1,53 @@
 # analysis_plots.R
 rm(list=ls())
 
+require(warningsignals)
+
+
+roc_effort_plot <- function(input, ...){
+# plots as a column
+  m <- length(input)
+  n <- length(input[[1]])
+  
+    par(mfrow=c(n,m), mar=c(0,0,0,0), oma=c(4,4,4,2))
+  for(j in 1:m){
+    n <- length(input[[j]])
+    for(i in 1:n){
+      plot_roc_curves(input[[j]][[i]], cex.axis=1.5, cex.lab=1.5, legend=F, lwd=3, ...)
+    }
+    mtext(names(input[[j]]),  NORTH<-3, cex=2, line=2, outer=TRUE) 
+  }
+}
+
 load("~/flickr/5910219566.Rdat")
+deut1 <- lapply(1:length(freq),
+                function(i) c(sampling[i], indicator_sampling[[i]]))
+
+
+load("~/flickr/5910198136.Rdat")
+drake <- lapply(1:length(freq),
+                function(i) c(sampling[i], indicator_sampling[[i]]))
+
+load("~/flickr/5906482315.Rdat")
+ibm <-  lapply(1:length(freq),
+                function(i) c(sampling[i], indicator_sampling[[i]]))
+
+load("~/flickr/5909491217.Rdat")
+deut3 <- lapply(1:length(freq),
+                function(i) c(sampling[i], indicator_sampling[[i]]))
+ 
+load("~/flickr/5909610015.Rdat")
+caco3 <-  lapply(1:length(freq),
+                function(i) c(sampling[i], indicator_sampling[[i]]))
+
+
+input <- list(Critical=ibm, Daphnia=drake, GlaciationI=deut1, GlaciationIII=deut3)
+
+
+source("analysis.R")
+png("rocs.png", width=3*4, units="in", height=8, res=400)
+roc_effort_plot(input, main="Glaciation I")
+dev.off()
 
 
 ###########################
@@ -9,34 +55,7 @@ require(socialR)
 script <- "analysis_plots.R"
 gitaddr <- gitcommit(script)
 tags="warningsignals, stochpop"
-tweet_errors(script, tags=tags)
 ###########################
-
-require(warningsignals)
-source("analysis.R")
-
-
-roc_effort_plot <- function(input, main=main, ...){
-# plots as a column
-  n <- length(input)
-
-  par(mfrow=c(n,1), mar=c(0,5,0,2), oma=c(4,0,4,0))
-    plot_roc_curves(input[[1]], cex.axis=1.5, cex.lab=1.5, cex.main=1.5, legend=F, lwd=4, xaxt="n",main=main, ...)
-  for(i in 2:(n-1)){
-    plot_roc_curves(input[[i]], cex.axis=1.5, cex.lab=1.5, legend=F, lwd=4, xaxt="n", ...)
-  }
-  plot_roc_curves(input[[n]], cex.axis=1.5, cex.lab=1.5, legend=F, lwd=4, ...)
-}
-
-
-
-input <- lapply(1:length(freq),
-                function(i) c(sampling[i], indicator_sampling[[i]]))
-
-png("rocs.png", width=2, units="in", height=8, res=400)
-roc_effort_plot(input, main="Glaciation I")
-dev.off()
-
 upload("rocs.png", script=script, gitaddr=gitaddr, tags=tags)
 
 
