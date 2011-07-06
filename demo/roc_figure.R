565b764ffe6115328cd401c1dc1b789bb505e561
Carl Boettiger <cboettig@gmail.com>
2011-07-06 14:33:13 -0700

    roc_figure.R  autocommit

--- demo/roc_figure.R
+++ demo/roc_figure.R
@@ -81,7 +81,7 @@ require(socialR)
 script <- "roc_figure.R"
 gitaddr <- gitcommit(script) # ok to do last since quick-run script
 tags="warningsignals, stochpop"
-upload("compare_rocs.png", script=script, gitaddr=gitaddr, tags=tags)
+#upload("compare_rocs.png", script=script, gitaddr=gitaddr, tags=tags)
 upload("roc_example.png", script=script, gitaddr=gitaddr, tags=tags)
-upload("roc_for_dummies.png", script=script, gitaddr=gitaddr, tags=tags)
+#upload("roc_for_dummies.png", script=script, gitaddr=gitaddr, tags=tags)
 
