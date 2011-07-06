a684e9397e937909830e17e2b97bee59e6730118
Carl Boettiger <cboettig@gmail.com>
2011-07-06 14:05:06 -0700

    analysis_plots.R  autocommit

--- demo/analysis_plots.R
+++ demo/analysis_plots.R
@@ -1,7 +1,7 @@
 # analysis_plots.R
 rm(list=ls())
 
-load("drake_ltc.Rdat")
+load("deut1.Rdat")
 
 ###########################
 require(socialR)
@@ -20,18 +20,18 @@ indicator_sampling_freq(m, cpu, nboot,
                         sample_effort=freq) 
 
 
-png("drake_ltc_roc.png"); plot_roc_curves(c(list(mc), taus)); dev.off()
-upload("drake_ltc_roc.png", script=script, gitaddr=gitaddr, tags=tags)
+png("deut1_roc.png"); plot_roc_curves(c(list(mc), taus)); dev.off()
+upload("deut1_roc.png", script=script, gitaddr=gitaddr, tags=tags)
 
 for(i in 1:length(freq)){
   input <- c(sampling[i], indicator_sampling[[i]])
-  file <- paste("drake_ltc_", freq[i], ".png", sep="")
+  file <- paste("deut1_", freq[i], ".png", sep="")
   png(file); 
   plot_roc_curves(input, cex.axis=2, cex.lab=2); 
   dev.off()
   upload(file, script=script, gitaddr=gitaddr, tags=tags)
 
-#  file <- paste("dist_drake_ltc_", freq[i], ".png", sep="")
+#  file <- paste("dist_deut1_", freq[i], ".png", sep="")
 #  png(file, width=480*length(input))
 #  plot_dists(input, cex.axis=3, cex.lab=3.5); 
 #  dev.off()
