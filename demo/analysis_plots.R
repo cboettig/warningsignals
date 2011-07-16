14d0a7d6964c1f79c0f45ca5a46776201b2767e5
Carl Boettiger <cboettig@gmail.com>
2011-07-16 16:19:24 -0700

    heading change on analysis_plots.R

--- demo/analysis_plots.R
+++ demo/analysis_plots.R
@@ -1,4 +1,6 @@
 # analysis_plots.R
+## Figure 4
+
 rm(list=ls())
 
 require(warningsignals)
@@ -25,28 +27,24 @@ roc_effort_plot <- function(input, freq, ...){
 sets <- c(1,2,4)
 
 load("~/flickr/5909491217.Rdat")
-deut3 <- lapply(1:length(freq),
+deut3 <- lapply(1:sets,
                 function(i) c(sampling[i], indicator_sampling[[i]]))
 load("~/flickr/5909610015.Rdat")
-caco3 <-  lapply(1:length(freq),
+caco3 <-  lapply(sets,
                 function(i) c(sampling[i], indicator_sampling[[i]]))
-
 load("~/flickr/5910219566.Rdat")
 deut1 <- lapply(sets,
                 function(i) c(sampling[i], indicator_sampling[[i]]))
-
-
 load("~/flickr/5910198136.Rdat")
 drake <- lapply(sets,
                 function(i) c(sampling[i], indicator_sampling[[i]]))
-
 load("~/flickr/5906482315.Rdat")
 ibm <-  lapply(sets,
                 function(i) c(sampling[i], indicator_sampling[[i]]))
 
 
 
-input <- list(Simulation=ibm, Daphnia=drake, GlaciationI=deut1)
+input <- list(Simulation=ibm, Daphnia=drake, Glaciation=deut3)
 
 
 source("analysis.R")
