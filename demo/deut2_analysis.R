b5af62cef96f2645dfe5e7a006a73458f89515f4
Carl Boettiger <cboettig@gmail.com>
2011-07-17 12:30:55 -0700

    deut2_analysis.R  autocommit

---
+++ demo/deut2_analysis.R
@@ -0,0 +1,26 @@
+# deut_analysis.R
+rm(list=ls())
+require(warningsignals)
+
+
+###############
+require(socialR)
+script <- "deut2_analysis.R"
+gitaddr <- gitcommit(script)
+tags="warningsignals, stochpop"
+###############
+source("analysis.R")
+m <- fit_models(ibm_critical, "LSN")
+
+data(deuterium)
+i <- 2 ## Which deut?
+sampling <- sampling_freq(m$const, m$timedep, cpu=cpu, nboot=nboot,
+                          sample_effort=freq)
+taus <- reformat_tau_dists(bootstrap_tau(m$X, m$const, m$timedep, 
+                                         cpu=cpu, nboot=nboot))
+mc <- remove_unconverged(montecarlotest(m$const, m$timedep, 
+                                        cpu=cpu, nboot=nboot))
+indicator_sampling <- indicator_sampling_freq(m, cpu, nboot,
+                                              sample_effort=freq) 
+
+save(list=ls(), file="deut2.Rdat")
