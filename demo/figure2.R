e68a46506422de89c18f35bdb104769ea13ee5b0
Carl Boettiger <cboettig@gmail.com>
2011-08-02 15:58:51 -0700

    figure2.R  autocommit

--- demo/figure2.R
+++ demo/figure2.R
@@ -12,7 +12,7 @@ data(drake)
 
 X <- list("(a) Stable"=ibm_stable, "(b) Deteriorating"=ibm_critical, "(c) Daphnia"=drake_deterior$H6, "(d) Glaciation III"=deuterium[[3]])
 
-cairo_pdf("Fig2.pdf", width=6,height=4.6)
+png("Fig2.png", width=6, units="in", height=4.6)
 all_indicators(X, indicators=c("Var", "Autocor", "Skew", "CV"), method="kendall", pval=FALSE)
 dev.off()
 
