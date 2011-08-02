8e567d9bc2ad99d01fd198fef6e10809bf531979
Carl Boettiger <cboettig@gmail.com>
2011-08-01 19:53:44 -0700

    ibm_modelchoice.R  autocommit

--- demo/ibm_modelchoice.R
+++ demo/ibm_modelchoice.R
@@ -4,10 +4,21 @@ require(socialR)
 script <- "ibm_modelchoice.R"
 gitaddr <- gitcommit(script)
 
+T<- 1000
+n_pts <- 500
+require(warningsignals)
+
+## Collapsing example parameters 
+pars = c(Xo = 730, e = 0.5, a = 100, K = 1000, h = 200, 
+    i = 0, Da = .09, Dt = 0, p = 2)
+
+## Run the individual based simulation
+sn <- saddle_node_ibm(pars, times=seq(0,T, length=n_pts))
+# format output as timeseries
+ibm_critical <- ts(sn$x1,start=sn$time[1], deltat=sn$time[2]-sn$time[1])
+
 cpu <- 16
 nboot <- 500
-## The analyses -- slow!
-data(ibms)
 lsn <- fit_models(ibm_critical, "LSN")
 ltc <- fit_models(ibm_critical, "LTC")
 mc <- remove_unconverged(montecarlotest(ltc$timedep, lsn$timedep, 
