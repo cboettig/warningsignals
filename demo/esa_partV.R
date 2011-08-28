20c984e95c1db6906dffc818d6471f0442ec6634
Carl Boettiger <cboettig@gmail.com>
2011-08-27 20:07:53 -0700

    local esa examples

--- demo/esa_partV.R
+++ demo/esa_partV.R
@@ -1,21 +1,17 @@
 require(warningsignals)
 load("false_negative.Rdat") # Failed Detection
 fn <- fit_models(ibm_critical, "LSN")
-fn_mc <- montecarlotest(fn$const, fn$test, cpu=16, nboot=200)
-fn_taus <- tau_dist_montecarlo(ibm_critical, fn$const, fn$timedep, nboot=500, cpu=16)
+fn_taus <- tau_dist_montecarlo(ibm_critical, fn$const, fn$timedep, nboot=500, cpu=16, signal="CV")
 load("true_positive.Rdat") # Successful Detection
 tp <- fit_models(ibm_critical, "LSN")
-tp_mc <- montecarlotest(tp$const, tp$test, cpu=16, nboot=200)
-tp_taus <- tau_dist_montecarlo(ibm_critical, tp$const, tp$timedep, nboot=500, cpu=16)
+tp_taus <- tau_dist_montecarlo(ibm_critical, tp$const, tp$timedep, nboot=500, cpu=16, signal="CV")
 
 load("small_tau.Rdat") # 
 tn <- fit_models(ibm_critical, "LSN")
-tn_mc <- montecarlotest(tn$const, tn$test, cpu=16, nboot=200)
-tn_taus <- tau_dist_montecarlo(ibm_critical, tn$const, tn$timedep, nboot=500, cpu=16)
+tn_taus <- tau_dist_montecarlo(ibm_critical, tn$const, tn$timedep, nboot=500, cpu=16, signal="CV")
 
 load("stable_pos.Rdat") # false alarm
 fp <- fit_models(ibm_critical, "LSN")
-fp_mc <- montecarlotest(fp$const, fp$test, cpu=16, nboot=200)
-fp_taus <- tau_dist_montecarlo(ibm_critical, fp$const, fp$timedep, nboot=500, cpu=16)
+fp_taus <- tau_dist_montecarlo(ibm_critical, fp$const, fp$timedep, nboot=500, cpu=16, signal="CV")
 
-save(list=ls(), file="partIV.Rdat")
+save(list=ls(), file="partV.Rdat")
