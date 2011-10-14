88d50eccdb86b78405ff6ed4896b00635bdc3664
Carl Boettiger <cboettig@gmail.com>
2011-10-14 12:47:21 -0700

    ibm_analysis.R  autocommit

--- demo/ibm_analysis.R
+++ demo/ibm_analysis.R
@@ -4,9 +4,7 @@ require(warningsignals)
 require(socialR)
 script <- "ibm_analysis.R"
 gitaddr <- gitcommit(script)
-tags="warningsignals, stochpop"
-tweet_errors(script, tags=tags)
-on.exit(system("git push")) 
+
 
 cpu <- 16
 nboot <- 500
@@ -27,30 +25,12 @@ mc <- remove_unconverged(montecarlotest(m$const, m$timedep,
 indicator_sampling <- indicator_sampling_freq(m, cpu, nboot,
                                               sample_effort=freq) 
 
+save(list=ls(), file="short_ibms.Rdat")
+
+
 ### Plot methods
 ## Original plot
 #png("ibm_crit_roc.png"); plot_roc_curves(c(list(mc), taus)); dev.off()
 #upload("ibm_crit_roc.png", script=script, gitaddr=gitaddr, tags=tags)
-
-#
-#for(i in 1:length(freq)){
-#  input <- c(sampling[i], indicator_sampling[[i]])
-#  file <- paste("ibm_crit_", freq[i], ".png", sep="")
-#  png(file); 
-#  plot_roc_curves(input, cex.axis=2, cex.lab=2); 
-#  dev.off()
-#  upload(file, script=script, gitaddr=gitaddr, tags=tags)
-#
-#  file <- paste("dist_ibm_crit_", freq[i], ".png", sep="")
-#  png(file, width=480*length(input))
-#  plot_dists(input, cex.axis=3, cex.lab=3.5); 
-#  dev.off()
-#  upload(file, script=script, gitaddr=gitaddr, tags=tags)
-#}
-
-
-
-
-## IBM STABLE MODEL 
 
 
