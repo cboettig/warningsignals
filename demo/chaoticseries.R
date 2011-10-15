6b7238a1d8c5103b0ea31ac0b05b8755f81f62b0
Carl Boettiger <cboettig@gmail.com>
2011-10-15 13:10:56 -0700

    chaoticseries.R  autocommit

---
+++ demo/chaoticseries.R
@@ -0,0 +1,18 @@
+N <- numeric(100)
+N[1] <- 50
+
+r <- 4
+K <- 100
+
+f <- function(N) r*N*(1-N/K)
+
+for(t in 1:99)
+	N[t+1] = f(N[t])
+
+png("chaos.png")
+plot(1:100, N)
+dev.off()
+
+require(socialR)
+upload("chaos.png", script="chaoticseries.R", tags="stochpop")
+
