ef38a3d2443af47cd44752b226dd4e268b0f9058
Carl Boettiger <cboettig@gmail.com>
2011-11-21 17:49:57 -0800

    local edits

--- demo/figure2.R
+++ demo/figure2.R
@@ -8,6 +8,6 @@ data(drake)
 X <- list("(a) Stable"=ibm_stable, "(b) Deteriorating"=ibm_critical, "(c) Daphnia"=drake_deterior$H6, "(d) Glaciation III"=deuterium[[3]])
 
 cairo_pdf("Fig2.pdf", width=6,  height=4.6)
-all_indicators(X, indicators=c("Var", "Autocor", "Skew", "CV"))
+all_indicators(X, indicators=c("Var", "Autocor", "Skew", "CV"), pval=F)
 dev.off()
 
