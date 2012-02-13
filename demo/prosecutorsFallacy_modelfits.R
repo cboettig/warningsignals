e65fc27bdc3c4470de5bc32bc3d5b31e6c754ba8
Carl Boettiger <cboettig@gmail.com>
2012-02-12 19:03:20 -0800

    getting farm running prosecutorFallacy model fits (test size)

--- demo/prosecutorsFallacy_modelfits.R
+++ demo/prosecutorsFallacy_modelfits.R
@@ -30,11 +30,11 @@ library(snow)
 cluster <- makeCluster(16, type="MPI")
 clusterEvalQ(cluster, library(earlywarning)) # load a library
 clusterExport(cluster, ls()) # export everything in workspace
-out <- parSapply(cluster, 1:16, function(x)
-  stability_model(dat[dat$rep==i, c("time", "population")], "Var")[1] 
+out <- parSapply(cluster, 1:16, function(i)
+  stability_model(dat[dat$rep==i, c("time", "population")], "LSN")[1] 
 )
 print(out)
 stopCluster(cluster)
 
-
+save(list=ls(), file="prosecutor_mpi.rda")
 
